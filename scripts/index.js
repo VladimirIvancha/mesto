1284d290881ab12979c1453f2b099e44adaf745c
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2022-02-16 00:32:33 +0300

    fixed all & PixelPerfect

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,26 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupEditButtonElement = document.querySelector('.profile__edit-button');
+let formElement = popupElement.querySelector('.form');
+const profileData = document.querySelector('.profile');
+let profileName = profileData.querySelector('.profile__title');
+let profileJob = profileData.querySelector('.profile__subtitle');
+let fieldNameData = document.getElementById('profile-name');
+let fieldNameJob = document.getElementById('profile-prophecy');
 const openEditProfilePopup = function() {
   popupElement.classList.add('popup_is-opened');
+  fieldNameData.value = profileName.textContent;
+  fieldNameJob.value = profileJob.textContent;
 };
 const closeEditProfilePopup = function() {
     popupElement.classList.remove('popup_is-opened');
 };
-// реализуем  заполнение формы - редактирование имени и инфо о себе;
-let formElement = document.querySelector('.form');
-const profileData = document.querySelector('.profile');
-const fieldNameData = document.getElementById('profile-name');
-const fieldNameJob = document.getElementById('profile-prophecy');
 function formSubmitHandler (evt) {
     evt.preventDefault();
     var userNameInput = fieldNameData.value;
     var userJobInput = fieldNameJob.value;
-    profileData.querySelector('.profile__title').textContent = userNameInput;
-    profileData.querySelector('.profile__subtitle').textContent = userJobInput;
+    profileName.textContent = userNameInput;
+    profileJob.textContent = userJobInput;
     closeEditProfilePopup();
 }
 formElement.addEventListener('submit', formSubmitHandler);
